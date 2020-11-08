2d0a1995c2c2bee11353091cd97b0faaaabf7739
kartik <kartikbhalla12@gmail.com>
2020-11-09 00:43:00 +0530

    safetyInstructions: update content
    Signed-off-by: kartik <kartikbhalla12@gmail.com>

--- src/components/home/safetyInstructions.tsx
+++ src/components/home/safetyInstructions.tsx
@@ -5,7 +5,11 @@ export interface SafetyInstructionsProps {}
 const SafetyInstructions: React.FC<SafetyInstructionsProps> = () => {
 	return (
 		<div onClick={() => console.log('heh')} className='safetyInstructions'>
-			COVID - 19 Safety Instructions
+			<p>
+				COVID - 19 Safety Instructions <span>•</span> No-contact delivery and
+				pickup <span>•</span> Proper sanitization throughout the process{' '}
+				<span>•</span> Regular checkup of our delivery teams
+			</p>
 		</div>
 	);
 };
