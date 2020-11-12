ed76c818b57b3316543c0f8d4487a153f82a1c66
kartik <kartikbhalla12@gmail.com>
2020-11-12 22:02:28 +0530

    blog: update content
    Signed-off-by: kartik <kartikbhalla12@gmail.com>

--- src/components/blog/blogPost.tsx
+++ src/components/blog/blogPost.tsx
@@ -23,7 +23,7 @@ const BlogPost: React.FC<BlogPostProps> = () => {
 				<h4>
 					8 Nov, 2020
 					<span className='author'>
-						By Lorem Ipsum <span>•</span> 3 minutes read
+						By Hiteshi Gangwani <span>•</span> 3 minutes read
 					</span>
 				</h4>
 				<div className='sharePost'>
@@ -62,16 +62,23 @@ const BlogPost: React.FC<BlogPostProps> = () => {
 						<Linkedin />
 					</div>
 				</div>
-				<img src='http://placekitten.com/800/800' alt='' />
+				<img src='/blog1.png' alt='' />
 
 				<p>
 					What if we told you that your world might not completely revolve
 					around cash anymore? What if you would not have to look at your purse
-					everytime you want something? Sounds like a dream, right? Well guess
+					every time you want something? Sounds like a dream, right? Well, guess
 					what, it is not a dream anymore! The solution to all your problems can
-					now be traced back to barter.
+					now be traced back to barter. Barter as a practice was initiated in
+					the 6000 BCE throughout the world. Even though it was resumed in the
+					1930s during the Great Depression in some parts of the world, it
+					largely remained discontinued in most of the parts of the world. All
+					of us can admit that our lives have been governed by cash in one way
+					or another, thanks to capitalism. The worst part is that it makes us
+					believe that there are no other options and that is partially the
+					reason why we view everything in terms of money.
 				</p>
-				<p>
+				{/* <p>
 					Barter as a practice, was initiated in the 6000 BCE throughout the
 					world. Even though it was resumed in the 1930s during the Great
 					Depression in some parts of the world, it largely remained
@@ -82,10 +89,10 @@ const BlogPost: React.FC<BlogPostProps> = () => {
 					way or another, thanks to capitalism. The worst part is that it makes
 					us believe that there are no other options and that is partially the
 					reason why we view everything in terms of money.
-				</p>
+				</p> */}
 				<p>
 					As an idea opposed to barter, cash has significantly become a deciding
-					factor for a lot of things in our day-to-day lives : the healthcare
+					factor for a lot of things in our day-to-day lives: the healthcare
 					facilities we get, the things we want, the things we eat, wear, and
 					many more. However, the pandemic has limited this deciding factor and
 					in turn, forced us to limit our wants.
@@ -105,9 +112,11 @@ const BlogPost: React.FC<BlogPostProps> = () => {
 					which provide us with immediate gratification but what we fail to
 					understand is that we are all in this together for a long time.
 					Instead of looking for quick, short term solutions to our problems, we
-					need to look for long term and assuring solutions which can help us
-					all in a sustainable manner because after all, we are all in this
-					together.
+					need to look for a long term and assuring solutions which can
+					sustainably help us all because after all, we are all in this
+					together. What we want for you is to stop limiting your wants and
+					start looking for sustainable alternatives to cash in order to fulfil
+					them. You guessed it right, one such alternative is Len Den.
 				</p>
 				<p>
 					What we want for you is to stop limiting your wants and start looking
@@ -125,11 +134,9 @@ const BlogPost: React.FC<BlogPostProps> = () => {
 					footprint for all those who want to take their first step towards a
 					sustainable life too. Len Den is here to encourage you to leave your
 					mark and start your barter journey. We hope that with time, all of us
-					can learn and unlearn to do things differently.
-				</p>
-				<p>
-					Remember that your choice to barter is going to matter, always.
-					Register your product, take your first step{' '}
+					can learn and unlearn to do things differently. Remember that your
+					choice to barter is going to matter, always. Register your product,
+					take your first step{' '}
 					<span
 						className='postProduct'
 						onClick={() => history.push('/post-product')}>
